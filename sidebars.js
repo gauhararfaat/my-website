bf86fd296c6052ff9922eab004632d3ac26169b0
gauhararfaat <49276106+gauhararfaat@users.noreply.github.com>
2020-09-24 12:47:55 +0530

    Add files via upload

---
+++ sidebars.js
@@ -0,0 +1,15 @@
+module.exports = {
+  someSidebar: {
+    Senate: ['doc1', 'doc2', 'Minutes',],
+    BoG: ['doc3','doc4','Minutes',],
+
+    FC: ['doc1', 'doc2', 'Minutes',],
+
+    Academics: ['doc1', 'doc2', 'Minutes',],
+    
+
+},
+
+};
+
+
