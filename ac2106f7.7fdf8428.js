e9a3f4f61e826bc0df51a2643a9c4584e64c0e72
Technical Officer <to@mailman.iitjammu.ac.in>
2020-09-30 13:07:38 +0530

    Deploy website - based on HEAD

---
+++ ac2106f7.7fdf8428.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[21],{75:function(p){p.exports=JSON.parse('{"allTagsPath":"/my-website/blog/tags","slug":"pg","name":"pg","count":2,"permalink":"/my-website/blog/tags/pg"}')}}]);
